8e9b1cdfbd640b4da4d77d77040d302db554e1cb
Ahmed Said <a.said1991@gmail.com>
2017-08-25 02:00:03 +0200

    fix validate method issue
     return false instead throwing expections

--- auth-0-evaluation-task.js
+++ auth-0-evaluation-task.js
@@ -1,14 +1,16 @@
 var express    = require('express');
 var Webtask    = require('webtask-tools');
 var bodyParser = require('body-parser');
-var base64_url_decode = require('base64-decode');
+var base64urlDecode = require('base64-decode');
 var crypto = require('crypto');
 
 // express settings
 var app = express();
 app.use(bodyParser.json());
 
+//constants
 const SECRET = 'auth0';
+const ALG = 'sha256';
 
 //token manager module
 var tokenManager = {
@@ -16,41 +18,41 @@ var tokenManager = {
   validateTokenFormat: function(token){
     //check token exist
     if (!token) {
-      throw new Error('Token not found');
+      //Token not found
+      return false;
     }
     // check segments
     var segments = token.split('.');
     if (segments.length !== 3) {
-      throw new Error('Not enough or too many segments');
+      //Not enough or too many segments
+      return false;
     }
+    
     return true;
   },
   //validate token signature
   validateSignature: function(token){
       var segments = token.split('.');
-      // base64 decode and parse JSON
-      var header = JSON.parse(base64urlDecode(segments[0]));
-      var payload = JSON.parse(base64urlDecode(segments[1]));
+      // base64 decode
+      var header = base64urlDecode(segments[0]);
+      var payload = base64urlDecode(segments[1]);
       
       //singature should be the same
       var signature = segments[2];
       
       // verify signature. `sign` will return base64 string.
-      var signingInput = [headerSeg, payloadSeg].join('.');
-      if (!verify(signingInput, key, signingMethod, signingType, signatureSeg)) {
-        throw new Error('Signature verification failed');
-      }
-      
+      var signingInput = [header, payload].join('.');
+      var hash = crypto.createHmac(ALG, SECRET).update(signingInput).digest('base64');
+      return hash === signature;
   },
   //read the token contents head & payload 
   resolveToken : function(token){
     //get segements
     var segments = token.split('.');
     //decode segments from base64 to readable format
-    var header = JSON.parse(base64_url_decode(segments[0]));
-    var payload = JSON.parse(base64_url_decode(segments[1]));
-    var jwt = 
-    {
+    var header = JSON.parse(base64urlDecode(segments[0]));
+    var payload = JSON.parse(base64urlDecode(segments[1]));
+    var jwt = {
       header : header,
       payload : payload
     };
@@ -59,16 +61,25 @@ var tokenManager = {
   }
 };
 
-
+//set the default route
 app.get('/', function (req, res) {
+  //get token value from query string
   var token = req.query.token;
-  if(!token)
-  {
+  if(!token){
       res.send("Please include a JWT token in \"token\" query string!");
   }
-  //resolve token
-  var jwt = tokenManager.resolveToken(token);
-  res.send(jwt);
+  else if(!tokenManager.validateTokenFormat(token)){
+      res.send("Invalid JWT format");
+  }
+  else if(!tokenManager.validateSignature(token)){
+      res.send("Failed validate JWT signature");
+  }
+  else{
+    //resolve token
+    var jwt = tokenManager.resolveToken(token);
+    res.send(jwt);
+  }
+  
 });
 
 module.exports = Webtask.fromExpress(app);
